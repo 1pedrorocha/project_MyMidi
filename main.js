e4a1b4f37f0231ee594fb639d20b85f46c26d47b
1pedrorocha <rocha.pedroaf@gmail.com>
2023-11-07 19:48:11 -0300

    Optmizies the javascript code

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-function tocaSom(som) {
-  document.querySelector(`#som_tecla_${som}`).load();
-  document.querySelector(`#som_tecla_${som}`).play();
+function playSound(idKey) {
+  document.querySelector(idKey).load();
+  document.querySelector(idKey).play();
+}
+
+let counter = 0;
+const keyList = document.querySelectorAll(".key");
+
+while (counter < keyList.length) {
+  const key = keyList[counter];
+  const keyName = key.classList[1];
+
+  console.log(keyName);
+
+  const idAudio = `#sound_${keyName}`;
+
+  console.log(idAudio);
+
+  key.onclick = function () {
+    playSound(idAudio);
+  };
+  counter++;
 }
