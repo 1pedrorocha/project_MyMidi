c5fd07a1ab614de974030bcd55f59458d75c441a
1pedrorocha <rocha.pedroaf@gmail.com>
2023-11-07 20:18:17 -0300

    Adjusts when pressing enter or space to play keys

--- main.js
+++ main.js
@@ -8,12 +8,21 @@ const keyList = document.querySelectorAll(".key");
 for (let i = 0; i < keyList.length; i++) {
   const key = keyList[i];
   const keyName = key.classList[1];
-
   const idAudio = `#sound_${keyName}`;
-
-  console.log(idAudio);
 
   key.onclick = function () {
     playSound(idAudio);
+  };
+
+  key.onkeydown = function (event) {
+    console.log(event.code === "Enter" || event.code === "Space");
+
+    if (event.code === "Enter" || event.code === "Space") {
+      key.classList.add("active");
+    }
+  };
+
+  key.onkeyup = function () {
+    key.classList.remove("active");
   };
 }
